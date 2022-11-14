7172f7521a732c92f5336fde5bc771ec89c89ed3
MelinaLabra <melina.lb04@gmail.com>
2022-11-14 19:45:18 -0300

    agrega funcion sumar

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 //git init: inicializa un repositorio
 //git add .: para hacer cambios en el repositorio
 let saludo = "hola";
+// git commit -m "  ": para hacer un commit
+//git log: para ver los commit
